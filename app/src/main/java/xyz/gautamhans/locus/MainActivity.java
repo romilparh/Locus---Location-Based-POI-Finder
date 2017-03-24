b883f19c7b423c2f5c4587c378e46d204852700a
Gautam <gautamhans1@gmail.com>
2017-03-24 23:58:10 +0530

    added navigation list items with respective icons

--- app/src/main/java/xyz/gautamhans/locus/MainActivity.java
+++ app/src/main/java/xyz/gautamhans/locus/MainActivity.java
@@ -80,17 +80,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_saved_places) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_reminders) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_settings) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_feedback) {
+
+        } else if (id == R.id.nav_help) {
 
         }
 
