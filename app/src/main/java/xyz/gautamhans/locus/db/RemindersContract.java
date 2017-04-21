d445fc0f36f7b8f9290ca2147715dfe1ed3d6af1
Gautam <gautamhans1@gmail.com>
2017-04-22 02:43:48 +0530

    Added 7.1.x Pixel Launcher App Shortcuts

---
+++ app/src/main/java/xyz/gautamhans/locus/db/RemindersContract.java
@@ -0,0 +1,8 @@
+package xyz.gautamhans.locus.db;
+
+/**
+ * Created by Gautam on 21-Apr-17.
+ */
+
+public class RemindersContract {
+}
---
+++ app/src/main/java/xyz/gautamhans/locus/entity/StoredReminder.java
@@ -0,0 +1,8 @@
+package xyz.gautamhans.locus.entity;
+
+/**
+ * Created by Gautam on 21-Apr-17.
+ */
+
+public class StoredReminder {
+}
