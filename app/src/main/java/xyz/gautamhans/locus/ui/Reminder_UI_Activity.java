70b36f4151c1cc118519c1049d595543327dd2eb
Gautam <gautamhans1@gmail.com>
2017-04-22 17:51:49 +0530

    Redesigned the New Reminder UI

--- app/src/main/java/xyz/gautamhans/locus/ui/Reminder_UI_Activity.java
+++ app/src/main/java/xyz/gautamhans/locus/ui/Reminder_UI_Activity.java
@@ -3,19 +3,30 @@ package xyz.gautamhans.locus.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import xyz.gautamhans.locus.R;
 
-public class Reminder_UI_Activity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener {
-    SeekBar seekBar1;
-    TextView textView;
+public class Reminder_UI_Activity extends AppCompatActivity{
+
+    private static final int RADIUS_MAX = 500;
+    private static final int DEFAULT_RADIUS = 50;
+    private TextView address_selected, radius_text;
+    private EditText task_title, task_description;
+    private ImageView chooseLocation;
+    private SeekBar mRadius;
+
+    private double mLatitude;
+    private double mLongtitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,37 +38,40 @@ public class Reminder_UI_Activity extends AppCompatActivity implements SeekBar.O
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        seekBar1 = (SeekBar) findViewById(R.id.seekBar1);
-        seekBar1.setOnSeekBarChangeListener(this);
-        textView = (TextView) findViewById(R.id.textview4);
-        seekBar1.setMax(10);
+        task_title = (EditText) findViewById(R.id.task_title_tv);
+        task_description = (EditText) findViewById(R.id.task_desc_tv);
+        address_selected = (TextView) findViewById(R.id.address_selected_tv);
+        radius_text = (TextView) findViewById(R.id.radius_text);
+        mRadius = (SeekBar) findViewById(R.id.radius_slider);
+        mRadius.setMax(RADIUS_MAX);
+        updateRadiusText();
+
+        mRadius.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                updateRadiusText();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
 
-    public void saveButtonPressed(View view) {
-        Context context = getApplicationContext();
-        CharSequence text = "Reminder Saved";
-        int duration = Toast.LENGTH_LONG;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-
-    }
-
-    public void hide(View view) {
-
-        TextView txtView = (TextView) findViewById(R.id.locationSelected);
-
-        //Toggle
-        if (txtView.getVisibility() == View.INVISIBLE)
-            txtView.setVisibility(View.VISIBLE);
-
-        //If you want it only one time
-        //txtView.setVisibility(View.VISIBLE);
-
+    private void updateRadiusText(){
+        int meters = mRadius.getProgress();
+        this.radius_text.setText(getString(R.string.radius_in_m, meters));
     }
 
     @Override
     public void onBackPressed() {
-//        super.onBackPressed();
         Intent intent = new Intent(Reminder_UI_Activity.this, Reminders.class);
         startActivity(intent);
     }
@@ -71,32 +85,4 @@ public class Reminder_UI_Activity extends AppCompatActivity implements SeekBar.O
         }
         return super.onOptionsItemSelected(item);
     }
-
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        Toast.makeText(getApplicationContext(), "seekbar progress: " + progress, Toast.LENGTH_SHORT).show();
-
-        try {
-            int min = 5;
-            if (progress < 1) {
-                textView.setText("1");
-            } else {
-                textView.setText(progress);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-        Toast.makeText(getApplicationContext(), "seekbar touch started!", Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
-        Toast.makeText(getApplicationContext(), "seekbar touch stopped!", Toast.LENGTH_SHORT).show();
-    }
-    
 }
